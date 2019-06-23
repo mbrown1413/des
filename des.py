59105d65a1dba31fd704c82adcfd739163378133
Edward Palmer <edward.palmer@gmail.com>
2019-06-23 09:31:33 -0700

    Add 3DES and file support to des.py
    python des.py -c 1234567890abcdef  d3001515557aadadd3001515557aadae
    python des.py -d 161f79c6910b9c2d d3001515557aadadd3001515557aadae
    -> 1234567890abcdef

--- des.py
+++ des.py
@@ -1,4 +1,3 @@
-
 import sys
 import os.path
 
@@ -153,6 +152,51 @@ def bits_to_pretty(bits, blocksize=8):
         (bittools.bits_to_binary_string(bits, blocksize), bittools.bits_to_hex(bits), len(bits))
     '''
 
+def bytes_from_file(filename):
+    """Returns groups of 64 bits from filename."""
+    bit_rows = []
+    with open(filename, "rb") as f:
+        while True:
+            byte = f.read(8)
+            if byte == "":
+                break
+            bits = bittools.ascii_to_bits(byte)
+            while len(bits) < 64:
+                bits = bits + [0,0,0,0,0,0,0,0]
+            log(bits)
+            bit_rows.append(bits)
+    return bit_rows
+
+def get_key(orig_key):
+    key = bittools.hex_to_bits(orig_key)
+    if len(key) == 56:
+        for i in [7, 15, 23, 31, 39, 47, 55, 63]:
+            key.insert(i, 0)
+    if len(key) != 64:
+        raise ValueError("Key " + orig_key + " Is not 64 chars when expanded")
+    return key
+
+def get_keys(key_string):
+    """Returns 1 or 3 key depending if DES or 3DES Keying Option 1 or 2."""
+    keys = []
+
+    if len(key_string) % 16 == 0:
+        subkey_length = 16
+    elif len(key_string) % 14 == 0:
+        subkey_length = 14
+    else:
+        raise ValueError('Expected key length for [%s] to be a multiple of 14 or 16, was %d' % (key_string, len(key_string)))
+    i = 0
+    while i + subkey_length <= len(key_string):
+        keys.append(get_key(key_string[i:i+subkey_length]))
+        i += subkey_length
+
+    if len(keys) == 2:
+        # 3DES Keying option 2. Key 3 = Key 1.
+        keys.append(keys[0])
+
+    return keys
+
 if __name__ == "__main__":
 
     from optparse import OptionParser
@@ -167,39 +211,69 @@ if __name__ == "__main__":
         default=False, help="Print details and intermediate steps of the DSA algorithm.")
     op.add_option("-a", "--ascii", dest="ascii", action="store_true",
         default=False, help="Convert input plaintext from ascii if encrypting, or convert resulting plaintext to ascii if decrypting.")
+    op.add_option("-f", "--file", dest="file",
+        default=False, help="Encrypts / decrypts the file into an .encrypted / .decrypted.")
     (options, args) = op.parse_args()
 
-    if len(args) < 2:
-        op.error("Not enough arguments")
-    elif len(args) > 2:
-        op.error("Too many arguments")
-    key = bittools.hex_to_bits(args[1])
+    if options.file:
+        if len(args) != 1:
+            raise ValueError("Expected 1 and only 1 arg in file mode!")
+        keys = get_keys(args[0])
+    else:
+        if len(args) < 2:
+            op.error("Not enough arguments")
+        elif len(args) > 2:
+            op.error("Too many arguments")
+        keys = get_keys(args[1])
 
     # text is plaintext if encrypting or ciphertext if decrypting
-    if options.ascii and not options.decrypt:
-        text = bittools.ascii_to_bits(args[0])
+    if options.file:
+        text = bytes_from_file(options.file)
     else:
-        text = bittools.hex_to_bits(args[0])
-    if len(text) != 64:
-        if options.decrypt:
-            op.error("ciphertext must be 16 hex digits")
+        if options.ascii and not options.decrypt:
+            text = bittools.ascii_to_bits(args[0])
         else:
-            op.error("plaintext must be 16 hex digits (or 8 ascii letters if using -a/--ascii)")
-    if len(key) != 64:
-        print key, len(key)
-        op.error("key must be 16 hex digits")
+            try:
+                text = bittools.hex_to_bits(args[0])
+            except ValueError:
+                op.error("ciphertext couldn't be converted from [%s]. Perhaps you want --ascii or --file mode?" % args[0])
+        if len(text) != 64:
+            if options.decrypt:
+                op.error("ciphertext must be 16 hex digits")
+            else:
+                op.error("plaintext must be 16 hex digits (or 8 ascii letters if using -a/--ascii)")
+        text = [text]
 
-    if options.verbose:
-        print_logs = True
-    else:
-        print_logs = False
+    print_logs = options.verbose
 
-    if options.decrypt:
-        result = dsa_decrypt(text, key)
-    else:
-        result = dsa_encrypt(text, key)
+    for round in range(len(keys)):
+        key = keys[round]
+        if len(key) != 64:
+            print key, len(key)
+            op.error("key %s must be 16 hex digits" % key)
 
-    if options.ascii and options.decrypt:
-        print bittools.bits_to_ascii(result)
-    else:
-        print bittools.bits_to_hex(result)
+        if round % 2 == 0:
+            decrypt = options.decrypt
+        else:
+            # 3DES is EDE / DED, so round 2 is opposite.
+            decrypt = not options.decrypt
+
+        result = []
+        for t in text:
+            if decrypt:
+                result += dsa_decrypt(t, key)
+            else:
+                result += dsa_encrypt(t, key)
+
+        if options.ascii and decrypt:
+            print bittools.bits_to_ascii(result)
+        else:
+            print bittools.bits_to_hex(result)
+        if options.file:
+            filename = options.file + '.' + str(round) + '.'
+            if decrypt:
+                filename += 'decrypted'
+            else:
+                filename += 'encrypted'
+            with open(filename, "wb") as f:
+                f.write(bittools.bits_to_ascii(result))
